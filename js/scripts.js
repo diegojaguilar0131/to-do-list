451b611236b133a478b388afd4e0d4375b663382
Diego Aguilar <diegojaguilar0131@gmail.com>
2017-06-12 16:02:12 -0700

    create functioning delete, check, and show detail buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
+function Task (name, detail){
+  this.name = name;
+  this.detail = detail;
+}
+
 $(document).on("submit", "form#todo-list", function(){
   event.preventDefault();
-  var newElement = $("#new-item").val();
-  $("#printed-list").append("<li class='item'>"+ newElement + "<button type='submit' class='btn btn-danger btn-sm active remove-btn' >-</button><button type='submit' class='btn btn-warning btn-sm active dash-btn'>x</button><button type='submit' class='btn btn-primary btn-sm active show-btn'>show</button></li>");
+  var newName = $("#new-item").val();
+  var newDetail = $("#new-detail").val();
+  var newTask = new Task(newName, newDetail);
+
+
+  $("#printed-list").append("<li class='item'>"+ newTask.name + "<button type='submit' class='btn btn-danger btn-sm active remove-btn' >-</button><button type='submit' class='btn btn-sm active dash-btn'>✅</button><button type='submit' class='btn btn-primary btn-sm active show-btn'>show</button><p class='item-details'>" + newTask.detail + "</p></li>");
   $("#printed-list").show();
 
 });
@@ -13,10 +22,10 @@ $(document).on("click", ".remove-btn",function() {
 
 $(document).on("click", ".dash-btn",function() {
   event.preventDefault();
-  $(this).parent().empty();
+    $(this).parent("li").append("<p>DONE</p>");
 });
 
 $(document).on("click", ".show-btn",function() {
   event.preventDefault();
-  $(this).parent().empty();
+  $(this).siblings("p").toggle();
 });
