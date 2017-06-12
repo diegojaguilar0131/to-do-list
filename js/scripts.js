4887ce7eae5e3f2ed2337ffc9b1cf8cfaa86ac5e
Diego Aguilar <diegojaguilar0131@gmail.com>
2017-06-12 15:16:20 -0700

    added function to to do list can add & remove item

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-  
+$(document).on("submit", "form#todo-list", function(){
+  event.preventDefault();
+  var newElement = $("#new-item").val();
+  $("#printed-list").append("<li class='item'>"+ newElement + "<button type='submit' class='btn btn-danger btn-sm active remove-btn' >-</button><button type='submit' class='btn btn-warning btn-sm active dash-btn'>x</button><button type='submit' class='btn btn-primary btn-sm active show-btn'>show</button></li>");
+  $("#printed-list").show();
+
+});
+
+$(document).on("click", ".remove-btn",function() {
+  event.preventDefault();
+  $(this).parent().empty();
+});
+
+$(document).on("click", ".dash-btn",function() {
+  event.preventDefault();
+  $(this).parent().empty();
+});
+
+$(document).on("click", ".show-btn",function() {
+  event.preventDefault();
+  $(this).parent().empty();
 });
